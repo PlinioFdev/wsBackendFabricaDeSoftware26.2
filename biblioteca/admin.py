2946d5d7199d9450cacca83afee5323eac9b3372
Plínio Targino <PLiniotargino13@hotmail.com>
2026-08-29 19:25:33 -0300

    feat: registra models no admin do Django

--- biblioteca/admin.py
+++ biblioteca/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Estante, Livro
+
+
+@admin.register(Estante)
+class EstanteAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'criada_em']
+    search_fields = ['nome']
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'autor', 'ano', 'estante']
+    list_filter = ['estante']
+    search_fields = ['titulo', 'autor', 'isbn']
